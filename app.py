f9419f9b086331f9632d862608b4c669ab7d85dc
David King <dsmk@bu.edu>
2025-11-04 01:14:59 -0500

    Make a change to the app and see if it deploys

--- app.py
+++ app.py
@@ -12,3 +12,7 @@ def home():
 def get_data(x, y):
     total = x + y  # Simulate data processing
     return f"<p>Sum of {x} and {y} is {total}</p>\n"
+
+@app.route('/health')
+def health_check():
+    return "<p>OK</p>\n", 200
